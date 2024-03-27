563e146406c4f6a8fdb8e2607a37c7c221248a21
DianaHutuleac <diana.hutuleac@student.upt.ro>
2024-03-27 21:57:45 +0200

    initial commit

---
+++ app/src/main/java/com/example/helloworldapplication/HelloWorldActivity.java
@@ -0,0 +1,14 @@
+package com.example.helloworldapplication;
+import android.app.Activity;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+public class HelloWorldActivity extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        GLSurfaceView view = new GLSurfaceView(this);
+        view.setRenderer(new HelloWorldRenderer());
+        setContentView(view);
+    }
+}
